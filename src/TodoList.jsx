d6455c99d7f37f2dc740cfee8f7dde2962557c6c
Gabriel Bressi <gabriel@C11-I5BLCMJN31J.dir.svc.accenture.com>
2024-06-12 17:13:25 -0300

    feat: add todolist component and css

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import "./TodoList.css";
 
 function TodoList() {
     return (
         <>
-            <h1>Hello World</h1>
+            <h1>Lista de Tarefas</h1>
+            <form action="">
+                <input type="text" placeholder="Adicione uma tarefa"/>
+                <button className="add" type="submit">Add</button>
+            </form>
+
+            <div className="listaTarefas">
+                <div className="item">
+                    <span>Tarefa exemplo</span>
+                    <button>Deletar</button>
+                </div>
+
+                <div className="item completo">
+                    <span>Tarefa exemplo</span>
+                    <button>Deletar</button>
+                </div>
+            </div>
         </>
     )
 }
